0b78880739426eefe4af87e7015b0a34734143cb
Chris Whitehouse <chris.whitehouse@hotmail.com>
2020-11-14 17:01:12 +0000

    Started creating a game class

--- lib/jumpers-for-goalposts.rb
+++ lib/jumpers-for-goalposts.rb
@@ -1,3 +1,10 @@
-def game(team1, team2)
-  return "Team 1 Wins!"
+Game = Class.new
+
+class Game
+  def play
+    return "Team 1 Wins!"
+  end
 end
+
+match = Game.new
+match.play
--- spec/jumpers-for-goalposts_spec.rb
+++ spec/jumpers-for-goalposts_spec.rb
@@ -1,7 +1,7 @@
-require './jumpers-for-goalposts'
+require 'jumpers-for-goalposts'
 
-describe 'game' do
+describe 'play' do
   it 'outputs a result of a game between two teams' do
-    expect(game("team1","team2")).to eq "Team 1 Wins!"
+    expect(Game.play).to eq "Team 1 Wins!"
   end
 end
